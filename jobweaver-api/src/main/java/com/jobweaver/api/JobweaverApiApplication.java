77bdae526f6620f65bf85f507378ffd9ea975de9
Chirag <chirag2006chopra@gmail.com>
2026-02-08 17:16:25 +0530

    add:Docker Setup
    Individual service containers still missing, will do later..

--- jobweaver-api/src/main/java/com/jobweaver/api/JobweaverApiApplication.java
+++ jobweaver-api/src/main/java/com/jobweaver/api/JobweaverApiApplication.java
@@ -1,4 +1,4 @@
-package com.jobweaver.jobweaverapi;
+package com.jobweaver.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- jobweaver-api/src/test/java/com/jobweaver/api/JobweaverApiApplicationTests.java
+++ jobweaver-api/src/test/java/com/jobweaver/api/JobweaverApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.jobweaver.jobweaverapi;
+package com.jobweaver.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
