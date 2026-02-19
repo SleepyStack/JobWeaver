1b492253d5b91345192fbccf43cc857a738008a6
SleepyStack <chirag2006chopra@gmail.com>
2026-02-19 18:27:03 +0800

    add: Job Service

--- jobweaver-api/src/main/java/com/jobweaver/api/entity/Job.java
+++ jobweaver-api/src/main/java/com/jobweaver/api/entity/Job.java
@@ -46,4 +46,30 @@ public class Job {
 
     private boolean cancelRequested;
 
+    public void markAsRunning(String workerId) {
+        if (this.status != JobStatus.QUEUED)
+            throw new IllegalStateException("Only QUEUED jobs can start");
+
+        this.status = JobStatus.RUNNING;
+        this.workerId = workerId;
+        this.startedAt = LocalDateTime.now();
+    }
+
+    public void markAsSuccess() {
+        if (this.status != JobStatus.RUNNING)
+            throw new IllegalStateException("Only RUNNING jobs can succeed");
+
+        this.status = JobStatus.SUCCESS;
+        this.completedAt = LocalDateTime.now();
+    }
+
+    public void markAsFailed(String error) {
+        if (this.status != JobStatus.RUNNING)
+            throw new IllegalStateException("Only RUNNING jobs can fail");
+
+        this.status = JobStatus.FAILED;
+        this.retryCount++;
+        this.lastError = error;
+        this.completedAt = LocalDateTime.now();
+    }
 }
---
+++ jobweaver-api/src/main/java/com/jobweaver/api/service/JobService.java
@@ -0,0 +1,10 @@
+package com.jobweaver.api.service;
+
+import com.jobweaver.api.entity.Job;
+
+public class JobService {
+    public void createJob(Job job){
+    
+        // publish job to message broker
+    }
+}
