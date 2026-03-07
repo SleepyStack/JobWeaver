fee5c6b673ed7bab869d001cb791ae8621c3130f
SleepyStack <chirag2006chopra@gmail.com>
2026-03-07 17:33:25 +0800

    add:Module for Auth Service
    Spring Boot Module and Dependencies for Spring Authorization Server

---
+++ jobweaver-auth/src/test/java/com/github/sleepystack/jobweaverauth/JobweaverAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sleepystack.jobweaverauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobweaverAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
